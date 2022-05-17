2affce3ca63d162ffc10328401e50ca005527c86
mairambek1302 <ulanbekov1302@gmail.com>
2022-05-17 19:28:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,130 @@
+// let maks = {
+//     name:'mairambek',
+//     age:'19',
+//     hobbese: {
+//         one:' play footbole',
+//         twoo:'sing song',
+
+//     }
+// }
+// console.log(Object.keys(maks))
+// console.log(Object.values(maks.hobbese))
+
+//функция
+// function viter( ){
+//     console.log(20 + 20)
+// }
+// viter()
+
+// function viter(a, b, c, ){
+//     console.log(a+b*c)
+// }
+// viter(15,12,56)
+
+
+
+// function viter( ){
+//     alert(20 + 20)
+// }
+// viter()
+
+// function viter( ){
+//     document.write(20 + 20)
+// }
+// viter()
+
+
+// let nam = [15,21,35,68 ]
+// function viter(ewgrgr){
+//     console.log(ewgrgr )
+// }
+// viter(nam)
+
+
+//эки жолу басканда иштейт
+// let button = document.getElementById('btn')
+// button.addEventListener('dblclick',func)
+
+// function func(){
+//     alert('кайра кайра баскылай бербечи')
+// }
+
+
+//мышканы алып келенде иштейт
+// let button = document.getElementById('btn')
+// button.addEventListener('mouseout',func)
+
+// function func(){
+//     alert('кайра кайра баскылай бербечи')
+// }
+
+
+// let button = document.getElementById('btn')
+// button.addEventListener('mouseout', func)
+
+// function func(){
+//     alert('кайра кайра баскылай бербечи')
+// }
+
+
+// let button = document.getElementById('btn')
+// button.addEventListener('mouseout', func)
+
+// function func(){
+//     alert('кайра кайра баскылай бербечи')
+// }
+
+
+let button = document.querySelector('.biti')z
+button.addEventListener('click',func)
+let p = document.getElementById('p')
+function func(){
+    p.style.display = none
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
